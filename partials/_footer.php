ad1815d9e98acbd3ce310919e9c5d7564fd96d50
alexander784 <you@example.com>
2024-06-11 09:31:18 +0300

    Add footer

---
+++ partials/_footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    <div class="footer">
+    <p>@2024 all right reserved Alexander Nyaga</p>
+    </div>
+</body>
+</html>
