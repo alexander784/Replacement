9885619f168f84ecd92e0be9e0e7df5e37700de4
alexander784 <you@example.com>
2024-06-12 10:17:07 +0300

    Add PHP script to fetch and display ID replacement requests

--- Admin/admin_dashboard.php
+++ Admin/admin_dashboard.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
+include('../config.php');
 
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
+$sql = "SELECT * FROM id_requests";
+$result = $mysqli->query($sql);
 
 ?>
+
+
